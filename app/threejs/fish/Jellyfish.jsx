e31c4372cbaf4dbebef63ddd1d1cbaa810c072c2
Kancjusz <komendant.lucky@gmail.com>
2025-08-28 20:30:28 +0200

    Jellyfish: synced movement & animation, made seamless textures

--- app/threejs/fish/Jellyfish.jsx
+++ app/threejs/fish/Jellyfish.jsx
@@ -2,7 +2,7 @@ import { useEffect, useMemo, useRef, useState } from 'react'
 import { useGLTF, useAnimations } from '@react-three/drei'
 import { SkeletonUtils } from 'three/examples/jsm/Addons.js';
 import { Group, LoopOnce, LoopRepeat, Vector3 } from 'three';
-import { useFrame } from '@react-three/fiber';
+import { useFrame, useThree } from '@react-three/fiber';
 
 export default function Jellyfish(props) {
   const { scene, materials, animations } = useGLTF('/models/jellyfish.glb')
@@ -10,7 +10,6 @@ export default function Jellyfish(props) {
   const clone = useMemo(() => SkeletonUtils.clone(scene), [scene]);
   const jellyfishRef = useRef(new Group());
   const didEffect = useRef(false);
-  const idle = useRef(true);
 
   const distance = useRef(0);
   const time = useRef(0);
@@ -29,48 +28,51 @@ export default function Jellyfish(props) {
       didEffect.current = true;
       return;
     }
-
-    if(index == 1)
-      setTimeout(()=>{
-        distance.current = jellyfishRef.current.position.y + goalDistance;
-        idle.current = false;
-      },9000)
-    else idle.current = true;
-
     const oppositeIndex = Math.abs(index-1)
-    const rand = Math.random() * oppositeIndex + 1;
+    const rand = Math.random() * oppositeIndex;
 
-    modelAnimations.actions[modelAnimations.names[index]].reset().fadeIn(2).play();
-
-    setTimeout(()=>{
+    const clip = modelAnimations.actions[modelAnimations.names[index]];
+    clip.setDuration(7);
+    clip.startAt(3 * rand);
+    clip.reset().fadeIn(2).play();
+  
+    const eventFunc = () => {
+      modelAnimations.actions[modelAnimations.names[index]].fadeOut(1.5 * oppositeIndex + 0.5);
       setIndex(oppositeIndex);
-    },15000 * rand)
+    }
 
-    return () => modelAnimations.actions[modelAnimations.names[index]].fadeOut(2);
+    clip.getMixer().addEventListener("loop",eventFunc);
+
+    return () => clip.getMixer().removeEventListener("loop",eventFunc);
 
   }, [index])
 
   useFrame(({clock})=>{
     let delta = clock.getDelta();
-    if(idle.current){
+    const clipTime = modelAnimations.actions[modelAnimations.names[1]].time;
+
+    let rotationY = Math.sin(clock.elapsedTime / 50) * delta * 0.5;
+    jellyfishRef.current.rotateY(rotationY);
+
+    if(index == 1 && clipTime >= 2.3){
+      let timeFactor = (1-(clipTime-2.3)/4);
+
+      let distanceFactor = Math.min(Math.abs(distance.current - jellyfishRef.current.position.y) / goalDistance,1);
+
+      const offsetY = delta * 8 * distanceFactor * timeFactor;
+
+      jellyfishRef.current.translateY(offsetY);
+    }else{
       let offsetX = Math.sin(clock.elapsedTime / 5) * delta * 0.5;
-      let offsetY = (Math.sin(clock.elapsedTime / 7)-1) * delta * 0.45;
-      let rotationY = Math.sin(clock.elapsedTime / 50) * delta * 0.5;
+      let offsetY = (Math.sin(clock.elapsedTime / 7)-1.3) * delta * 0.45;
 
       jellyfishRef.current.translateX(offsetX);
       jellyfishRef.current.translateY(offsetY);
 
-      jellyfishRef.current.rotateY(rotationY);
 
+      distance.current = jellyfishRef.current.position.y + goalDistance;
       time.current = clock.elapsedTime;
-    }else{
-      let timeFactor = (1-(clock.elapsedTime - time.current)/6);
 
-      console.log(clock.elapsedTime - time.current);
-
-      let distanceFactor = Math.min(Math.abs(distance.current - jellyfishRef.current.position.y) / goalDistance,1);
-
-      jellyfishRef.current.translateY(delta * 8 * distanceFactor * timeFactor);
     }
   },[])
 
