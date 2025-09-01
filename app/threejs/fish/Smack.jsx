35fa1baa59257360e5441459b025a9d67832e298
Kancjusz <komendant.lucky@gmail.com>
2025-09-01 19:16:31 +0200

    improved jellyfish spacing

--- app/threejs/fish/Smack.jsx
+++ app/threejs/fish/Smack.jsx
@@ -11,15 +11,16 @@ export default function Smack({position, scale, depth, size, count=5, sceneHeigh
 
     const docHeight = document.documentElement.scrollHeight;
 
-    const calculatePosInBounds = (cameraDepth) => {
+    const calculatePosInBounds = (cameraDepth,index) => {
         const width = Math.tan((camera.fov/360) * Math.PI)*Math.abs(cameraDepth) * 2;
         const height = width * (docHeight/window.innerWidth);
 
         const boundaries = {x:width*2*size.x, y:(sceneHeight + height)*size.y};
+        const side = index % 2;
 
         return {
             pos: new Vector3(
-                position[0] + boundaries.x * (Math.random() * 2 - 1),
+                position[0] + boundaries.x * (Math.random() - 1 * side),
                 position[1] + boundaries.y * (Math.random() * 2 - 1),
                 camera.position.z - cameraDepth
             ),
@@ -32,7 +33,7 @@ export default function Smack({position, scale, depth, size, count=5, sceneHeigh
     const jellyfishTransforms = useMemo(()=>{
         return new Array(count).fill().map((_,i)=>({
             pos: (posAndBounds = calculatePosInBounds(
-                camera.position.z - (position[2] + depth * (Math.random() * depthDivider - depth + depthDivider * i))
+                camera.position.z - (position[2] + depth * (Math.random() * depthDivider - depth + depthDivider * i)), i
             )).pos,
             offsetIdle: new Vector2(0,0),
             rotationOffset: {value:0},
@@ -85,8 +86,6 @@ export default function Smack({position, scale, depth, size, count=5, sceneHeigh
             e.offsetIdle.setY(offsetY);
 
             e.rotationOffset.value = Math.sin((clock.elapsedTime + 123456789 * e.rand.value) / 50) * delta * 0.5 * e.rand.value;
-
-            //e.pos.add(new Vector3(offsetX,offsetY,0));
         })
     });
 
